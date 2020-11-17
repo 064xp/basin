03abdccf9022a62ea21f43b4d7cad7e388bcbd53
Paulo <paulozamora2000@gmail.com>
2020-11-16 20:47:53 -0600

    Implemented sign up route

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,15 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
+from werkzeug.security import generate_password_hash
+from sqlite3 import Error
 
 from modules.dataBase import DataBase
 
 app = Flask(__name__)
+dbFile = './dev.db'
 
 @app.route('/')
 def root():
     return 'you made it!'
 
+@app.route('/sign-up', methods=['POST'])
+def signUp():
+    db = DataBase(dbFile)
+    try:
+        json = request.get_json()
+        username = json['username']
+        password = json['password']
+    except:
+        return jsonify({'error': 'Invalid JSON'}), 400
+
+    try:
+        db.insertUser(username, generate_password_hash(password))
+    except Error as e:
+        return 'DB Error', 500
+    return username, 200
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(15)
-    db = DataBase('./dev.db')
+    db = DataBase(dbFile).createTables()
     app.run()
--- backend/src/modules/dataBase.py
+++ backend/src/modules/dataBase.py
@@ -1,22 +1,32 @@
 import sqlite3
 from sqlite3 import Error
+import uuid
 
 class DataBase:
     def __init__(self, dbFile):
         try:
             self.conn = sqlite3.connect(dbFile)
             self.cursor = self.conn.cursor()
-            self._createTables()
         except Error as e:
             print(e)
 
-    def _createTables(self):
+    def createTables(self):
         command = '''
         CREATE TABLE IF NOT EXISTS Users(
-            name text NOT NULL,
-            username text  NOT NULL PRIMARY KEY,
-            password text NOT NULL
+            username text  NOT NULL,
+            password text NOT NULL,
+            userId text NOT NULL PRIMARY KEY
         )
         '''
 
         self.cursor.executescript(command)
+
+    def insertUser(self, username, password):
+        command = '''
+        INSERT INTO Users
+        (username, password, userId)
+        VALUES (?, ?, ?)
+        '''
+        id = str(uuid.uuid4())
+        self.cursor.execute(command, (username, password, id))
+        self.conn.commit()
