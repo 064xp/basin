36e8400a35710ebe57b2b9fdfe81d0cb6ece4bb8
Paulo <paulozamora2000@gmail.com>
2020-11-21 17:11:12 -0600

    Made simple header.

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="header_user-name">User 1</div>
+      <div className="header_user-icon">AA</div>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
